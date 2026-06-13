d2577507e8f00b05174535b1050e806b33df6fa4
Nidhisha00 <nidhisharaj1204@gmail.com>
2026-06-13 21:38:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -163,5 +163,22 @@ window.addEventListener("scroll", () => {
         }
 
     });
+window.addEventListener("load", () => {
 
+    setTimeout(() => {
+
+        const preloader =
+            document.getElementById("preloader");
+
+        preloader.style.opacity = "0";
+
+        setTimeout(() => {
+
+            preloader.style.display = "none";
+
+        },1000);
+
+    },3000);
+
+});
 });
